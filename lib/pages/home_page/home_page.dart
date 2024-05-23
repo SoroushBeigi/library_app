7a80c4290c48e0c7fa5c98a621c8e0c792d80105
Soroush Beigi <sbeygi712@gmail.com>
2024-05-23 20:12:51 +0430

    creating homepage tabs

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -7,23 +7,53 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: DefaultTabController(
-        length: 3,
+        length: 4,
         child: Scaffold(
           appBar: AppBar(
             bottom: const TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(
+                  icon: Column(
+                    children: [
+                      Icon(Icons.book),
+                      Text('Books'),
+                    ],
+                  ),
+                ),
+                Tab(
+                  icon: Column(
+                    children: [
+                      Icon(Icons.person),
+                      Text('Members'),
+                    ],
+                  ),
+                ),
+                Tab(
+                  icon: Column(
+                    children: [
+                      Icon(Icons.home_work),
+                      Text('Publishers'),
+                    ],
+                  ),
+                ),
+                Tab(
+                  icon: Column(
+                    children: [
+                      Icon(Icons.emoji_people),
+                      Text('Employees'),
+                    ],
+                  ),
+                ),
               ],
             ),
-            title: const Text('Tabs Demo'),
+            title: const Text('Home Page'),
           ),
           body: const TabBarView(
             children: [
               Icon(Icons.directions_car),
               Icon(Icons.directions_transit),
               Icon(Icons.directions_bike),
+              Icon(Icons.airline_seat_flat_sharp),
             ],
           ),
         ),
