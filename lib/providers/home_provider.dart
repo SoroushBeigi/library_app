c02bd79f3526cf5fc02e06d8f15aca766f3b6e2d
s.masoumbeigi <sbeygi712@gmail.com>
2024-05-25 13:55:50 +0330

    adding mock book data

--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -5,12 +5,31 @@ import 'package:library_app/services/dio.dart';
 class HomeProvider extends ChangeNotifier {
   final _apiService = ApiService();
 
-  bool isLoaing=true;
-  List<BookModel> books=[];
+  bool isLoaing = true;
+  List<BookModel> books = [];
 
   Future<void> getBooks() async {
-    List<BookModel>? result = await _apiService.getBooks();
-    books = result ?? [];
-    isLoaing=false;
+    // List<BookModel>? result = await _apiService.getBooks();
+    // books = result ?? [];
+    Future.delayed(const Duration(seconds: 2)).then(
+      (_) => books = [
+        BookModel(
+          id: 1,
+          name: 'The books name is felan',
+          genre: 'science finction',
+          price: 20000,
+          takenBy: 'Soroush Beigi',
+        ),
+        BookModel(
+          id: 2,
+          name: 'The second books name is felan',
+          genre: 'science bla bla',
+          price: 150000,
+          takenBy: 'Mohammad Beigi',
+        ),
+      ],
+    );
+    isLoaing = false;
+    notifyListeners();
   }
 }
